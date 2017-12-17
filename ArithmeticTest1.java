de76b0372f38a9d6fcbe341df10129b938e4c95a
xsccccy <1354270697@qq.com>
2017-12-17 18:43:20 +0800

    Update ArithmeticTest1.java

--- ArithmeticTest1.java
+++ ArithmeticTest1.java
@@ -1,11 +1,3 @@
-import java.util.Random;
-import java.util.Date;
-import java.io.*;
-public class ArithmeticTest1
-{
-	public static void main (String[] args) 
-    {
-    	System.out.println ("_______________________________________");
-	    System.out.println ("Hey!I am Zjy");
-    }
-}
+#-*-coding:utf-8 -*-
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] 
+print(bicycles[0].title())
